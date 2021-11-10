c01644867a8655ad271fefcfe9213afd460c5029
james4-daf <d.james@elsevier.com>
2021-11-10 11:22:37 +0000

    1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>How About Them Apples</h1>
+      <form></form>
+    </div>
+  );
+}
+
+export default App;
