14bdbfca522c77647f3e51724dc034130d93f7fd
james4-daf <d.james@elsevier.com>
2021-11-11 12:43:31 +0000

    1.02

--- my-app/src/components/EverythingForms/Form.js
+++ my-app/src/components/EverythingForms/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Modal from "react-bootstrap/Modal";
+//import Modal from "react-bootstrap/Modal";
+import Axios from "axios";
 import "./Form.css";
 
 export class Form extends React.Component {
@@ -10,17 +11,41 @@ export class Form extends React.Component {
       searchQuery: "",
     };
   }
-
   handleSubmit = (event) => {
     event.preventDefault();
     alert("You have submitted the form.");
   };
+  getCrypto = () => {
+    // header("Access-Control-Allow-Origin", "http://localhost:8080");
+    // Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT");
+    Axios.get(
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+      {
+        method: "GET",
+        qs: {
+          start: "1",
+          limit: "10",
+          convert: "USD",
+        },
+        headers: {
+          "X-CMC_PRO_API_KEY": "57e0cad9-e1fd-4449-8834-dff39ba52a02",
+        },
+        json: true,
+        gzip: true,
+      }
+    ).then((response) => {
+      console.log(response);
+      SubtleCrypto(response.data.id);
+    });
+  };
 
   render() {
+    // const [crypto, setCrypto] = useState("");
     return (
       <div className="wrapper">
         <div>
           <h1>Crypto Investment Tracker</h1>
+          <button onClick={this.getCrypto}>Add</button>
           <button type="submit">Add Transaction</button>
           <form onSubmit={this.handleSubmit}>
             <fieldset>
